07e5147a5f0d83fef5ffe890379d3250c4652bc0
Adeleke Bright <bigjaratech@gmail.com>
2023-07-31 05:26:50 +0100

    Add Global Error Filtering

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalErrorFilter } from './common/global-error-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new GlobalErrorFilter())
   await app.listen(3000);
 }
 bootstrap();
