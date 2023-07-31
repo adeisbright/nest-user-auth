729b8f1384c3508ef5f92bbba80ed2d3d054cc0d
Adeleke Bright <bigjaratech@gmail.com>
2023-07-31 05:12:34 +0100

    Add Object Validation for User Body for Registration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Post, UsePipes , Body, InternalServerErrorException, HttpStatus } from "@nestjs/common";
+import { ValidateUserPipe } from "src/common/middleware/validate-user-body";
+import { UserDTO } from "src/user/user.dto";
 import { UserService } from "src/user/user.services";
 
 @Controller("auth")
@@ -9,8 +11,20 @@ export class AuthController {
     ) { } 
 
     @Post("/sign-up")
-    async handleRegistration() {
-        return "Sign up not implemented yet"
+    @UsePipes(ValidateUserPipe)
+    async handleRegistration(
+        @Body() body : UserDTO
+    ) {
+        const user = await this.userService.create(body) 
+        if (!user) {
+            throw new InternalServerErrorException("Server Error")
+        }
+        return {
+            data: user,
+            success: true,
+            message: "Registration Successful", 
+            statusCode : HttpStatus.CREATED
+        }
     }
 
     @Post("login")
