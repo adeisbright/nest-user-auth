f13200350794609070a3d1a2461d94707ba5be3e
Adeleke Bright <bigjaratech@gmail.com>
2023-08-02 16:47:33 +0100

    Use Dependency Injection for Reading env values

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import config from './config';
-
 @Module({
   imports: [
     UserModule, 
-    AuthModule, 
-    ConfigModule.forRoot({ 
-      load : [config],
-      isGlobal: true, 
-      cache : true 
-    })
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
